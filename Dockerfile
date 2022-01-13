4c81a7c430d49649ff3753307ab1f38e8abd1301
QFGlenn <glenn.h.lusk@gmail.com>
2022-01-12 16:53:18 -0800

    Fixed an issue where the secret key was compromised.

--- Dockerfile
+++ Dockerfile
@@ -10,9 +10,9 @@ RUN apk add --update --no-cache --virtual .tmp-build-deps \
 RUN pip install -r /requirements.txt
 RUN apk del .tmp-build-deps
 
-RUN mkdir /app
-WORKDIR /app
-COPY ./app /app
+RUN mkdir /collectible-app
+WORKDIR /collectible-app
+COPY ./collectible-app /collectible-app
 
 RUN adduser -D user
 USER user
--- collectible-app/base/admin.py
+++ collectible-app/base/admin.py
@@ -10,19 +10,21 @@ class UserAdmin(BaseUserAdmin):
     fieldsets = (
         (None, {
             'fields': ('email', 'password')}),
-        (_('Personal Info'),
+        (
+            _('Personal Info'),
             {'fields': ('name',)}),
         (
             _('Permissions'),
-            {'fields': ('is_active', 'is_staff', 'is_superuser')}
-            ), (_('Important Dates'), {'fields': ('last_login',)})
+            {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        (
+            _('Important Dates'),
+            {'fields': ('last_login',)})
     )
 
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2')
-        }),
+            'fields': ('email', 'password1', 'password2')}),
     )
 
 
